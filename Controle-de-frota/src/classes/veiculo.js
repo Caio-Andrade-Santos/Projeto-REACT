7e036ed44cb495243e29e259c18ec84f66a97c44
Caio-Andrade-Santos <cavians2015@gmail.com>
2025-11-26 20:52:44 -0300

    mudando propriedades

--- Controle-de-frota/src/classes/veiculo.js
+++ Controle-de-frota/src/classes/veiculo.js
@@ -3,18 +3,13 @@ class Veiculo {
     modelo = "Modelo não informado",
     placa = "XXX-0000",
     tipo = "Carga",
-    ano = 2024,
-    cor = "Indefinido",         // 🔥 nova propriedade
-    km = 0                      // 🔥 nova propriedade
+    ano = 2024
   ) {
     this.id = Date.now();
     this.modelo = modelo;
     this.placa = placa;
     this.tipo = tipo;
     this.ano = ano;
-
-    this.cor = cor;             // 🔥 salva nova prop
-    this.km = km;               // 🔥 salva nova prop
   }
 }
 
