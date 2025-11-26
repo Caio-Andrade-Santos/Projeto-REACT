bc8e26ef851ccebb91c5671275e73199d4fe3c27
Caio-Andrade-Santos <cavians2015@gmail.com>
2025-11-26 20:47:03 -0300

    adicionando required

--- Controle-de-frota/src/classes/veiculo.js
+++ Controle-de-frota/src/classes/veiculo.js
@@ -3,13 +3,18 @@ class Veiculo {
     modelo = "Modelo não informado",
     placa = "XXX-0000",
     tipo = "Carga",
-    ano = 2024
+    ano = 2024,
+    cor = "Indefinido",         // 🔥 nova propriedade
+    km = 0                      // 🔥 nova propriedade
   ) {
     this.id = Date.now();
     this.modelo = modelo;
     this.placa = placa;
     this.tipo = tipo;
     this.ano = ano;
+
+    this.cor = cor;             // 🔥 salva nova prop
+    this.km = km;               // 🔥 salva nova prop
   }
 }
 
